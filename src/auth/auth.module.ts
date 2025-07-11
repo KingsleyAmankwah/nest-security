37279bfedc7eb7efee31c3db62ec4ca36c8d1ee1
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-07-11 16:01:37 +0000

    feat(auth): add AuthModule with JWT, mailing, user repository, and config integration
    - Setup AuthModule with TypeORM User entity repository
    - Configure JwtModule asynchronously using app config
    - Configure MailerModule asynchronously for email support
    - Register AuthController and AuthService for auth logic and request handling

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig } from 'src/config/jwt.config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { mailerConfig } from 'src/config/mailer.config';
 
 @Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: jwtConfig,
+      inject: [ConfigService],
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: mailerConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
