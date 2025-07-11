0c5ecd4f5aef41c7186fba2df0e23f80cbc8ccdb
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-07-11 12:13:13 +0000

    refactor: remove unused DTOs and configuration files for Auth and database

--- src/cors.config.ts
+++
@@ -1,8 +0,0 @@
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { ConfigService } from '@nestjs/config';
-
-export const corsConfig = (configService: ConfigService): CorsOptions => ({
-  origin: configService.get<string>('FRONTEND_URL', 'http://localhost:4200'),
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  credentials: true,
-});
