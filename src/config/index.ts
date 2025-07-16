546b10b13c81f2f620a5a69d4b7f0b8ab311584a
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-07-16 14:50:30 +0000

    feat: add an entry file to export

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export * from './app-config.service';
+export * from './app-config.module';
+export * from './cors.config';
+export * from './jwt.config';
+export * from './mailer.config';
+export * from './database.config';
+export * from './app-config.module';
