57e85efed3219f05228e7832984f981201608037
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-07-16 14:51:21 +0000

    refactor: Update cors config

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,8 +1,11 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { ConfigService } from '@nestjs/config';
+import { AppConfigService } from './app-config.service';
 
-export const corsConfig = (configService: ConfigService): CorsOptions => ({
-  origin: configService.get<string>('FRONTEND_URL', 'http://localhost:4200'),
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+export const corsConfig = (configService: AppConfigService): CorsOptions => ({
+  origin: configService.isDevelopment
+    ? true
+    : ['https://domain.com', 'https://www.domain.com'],
   credentials: true,
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 });
