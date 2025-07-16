de798165713d86a9f074fba53dfd8bd2cecd759a
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-07-16 14:51:58 +0000

    update jwt config

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,23 +1,11 @@
-import { ConfigService } from '@nestjs/config';
 import { JwtModuleOptions } from '@nestjs/jwt';
+import { AppConfigService } from './app-config.service';
 
-/*
-  A function that returns the JwtModule configuration object.
-  It uses the ConfigService to read environment variables or fallback defaults,
-  setting up JWT signing options.
-
-  Parameters:
-    - configService: an instance of ConfigService to access environment variables.
-
-  Returns:
-    - An object conforming to JwtModuleOptions, which NestJS uses to configure JwtModule.
-
-  Details:
-    - 'secret' specifies the JWT signing secret.
-    - 'signOptions' sets the token expiration time (15 minutes for access tokens).
-*/
-
-export const jwtConfig = (configService: ConfigService): JwtModuleOptions => ({
-  secret: configService.get<string>('JWT_SECRET', '??'),
-  signOptions: { expiresIn: '15m' },
+export const jwtConfig = (
+  configService: AppConfigService,
+): JwtModuleOptions => ({
+  secret: configService.jwtSecret,
+  signOptions: {
+    expiresIn: configService.jwtExpiresIn,
+  },
 });
