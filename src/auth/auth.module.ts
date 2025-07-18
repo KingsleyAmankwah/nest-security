3d108106c385ecf1114db21ef14d0f8018ac32cd
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-07-18 12:10:59 +0000

    refactor: update auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,26 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConfig } from 'src/config/jwt.config';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { mailerConfig } from 'src/config/mailer.config';
+import { AppConfigService } from 'src/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({
-      imports: [ConfigModule],
       useFactory: jwtConfig,
-      inject: [ConfigService],
+      inject: [AppConfigService],
     }),
     MailerModule.forRootAsync({
-      imports: [ConfigModule],
       useFactory: mailerConfig,
-      inject: [ConfigService],
+      inject: [AppConfigService],
     }),
   ],
   controllers: [AuthController],
