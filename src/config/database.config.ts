c2f28e81be683fd2b9c522769f4b69032dd8dc88
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-07-22 14:51:06 +0000

    fix: add comment to clarify entity loading in TypeORM configuration

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -10,7 +10,7 @@ export const typeOrmConfig = (
   username: configService.databaseUser,
   password: configService.databasePassword,
   database: configService.databaseName,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'], //This allows TypeORM to automatically load all entity files, no matter where they are in the project
   synchronize: configService.isDevelopment, // When set to true, TypeORM automatically syncs your database schema with your entities every time the app starts.
   logging: configService.isDevelopment, // 'logging' is enabled only in development mode to help debug queries.
   ssl: configService.isProduction ? { rejectUnauthorized: false } : false,
